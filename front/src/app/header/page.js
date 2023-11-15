108ce1802135544003cede5ab70ce85f60c9a56f
Мөнхсайхан Баярсайхан <HP231097@G303-CC0155.local>
2023-11-15 18:02:17 +0800

    first

---
+++ front/src/app/header/page.js
@@ -0,0 +1,26 @@
+export default function Header(params) {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src="logo.jpeg" width={122} height={130} />
+        <h2 className="Poster-name">Quiz</h2>
+      </div>
+      <div>
+        <img
+          src="galaxy_303714-4260.avif"
+          alt="galaxy"
+          width={80}
+          height={80}
+          className="gal-img"
+        />
+      </div>
+      <div>
+        <div>
+          <li>login/</li>
+          <p>sing</p>
+        </div>
+        <li></li>
+      </div>
+    </div>
+  );
+}
---
+++ front/src/app/page.js
@@ -0,0 +1,8 @@
+import Header from "./header/page";
+export default function Quiz(parms) {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
